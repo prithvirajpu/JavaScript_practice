fec470a0bbb5c1bb714fa36564ef19172ebaea26
prithviraj <prithvirajpu999@gmail.com>
2025-09-25 13:34:59 +0530

    some functions

--- practice_different_concepts.js
+++ practice_different_concepts.js
@@ -16,3 +16,24 @@ switch (day){
     case 5:
         console.log('Friday')
 }
+
+// Function
+function total(a,b){
+    console.log(a+b)
+}
+total(10,20)
+// Adding the function to a variable 
+const multiple=function(a,b){
+    console.log(a*b) 
+}
+multiple(10,2)
+
+// Arrow function
+const arrow =(a,b)=>{
+    return a+b
+}
+console.log(arrow(10,2))
+
+// Arrow function with single line of code
+const single_line=(x,y)=>x*y
+console.log(single_line(10,9))
